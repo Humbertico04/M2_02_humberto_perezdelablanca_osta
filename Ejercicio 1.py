08dfb8c07e5bba75999be7be4d22c05f738c8937
Humbertico04 <113980694+Humbertico04@users.noreply.github.com>
2022-09-23 09:21:31 +0000

    Ejercicio 1

---
+++ Ejercicio 1.py
@@ -0,0 +1,10 @@
+var1=1
+var2=2
+var3=3
+var4=4
+var5=5
+variables=[var1,var2,var3,var4,var5]
+
+print(sum(variables)/len(variables))
+#Calculo la media artmética sumando todas las varibles y diviendo por el número de ellas
+#"La nota media es 3.0"
